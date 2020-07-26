579a30098959d2df65bc3cb37892c71c6de79807
Angel M. Montilla Jiménez <43794876+angelmontilla@users.noreply.github.com>
2020-07-26 13:59:01 +0200

    First upload
    Project just created

---
+++ src/main/java/angel/roshambo/first/FirstApplication.java
@@ -0,0 +1,13 @@
+package angel.roshambo.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/angel/roshambo/first/FirstApplicationTests.java
@@ -0,0 +1,13 @@
+package angel.roshambo.first;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
