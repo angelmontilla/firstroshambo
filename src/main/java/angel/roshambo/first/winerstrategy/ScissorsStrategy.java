5e9f5113e21ad6f42afefbc127492e5f70b49171
angelmontilla <angmontilla@gmail.com>
2020-07-28 21:28:19 +0200

    27/7/2020
    Strategy done

---
+++ src/main/java/angel/roshambo/first/winerstrategy/ScissorsStrategy.java
@@ -0,0 +1,39 @@
+/*
+ * First RoShamBo
+ * Creado el 28-jul-2020 a las 19:22:31
+ */
+package angel.roshambo.first.winerstrategy;
+
+/**
+ * <strong>ScissorsStrategy</strong> Is the Strategy for knowing winner from Scissors
+ * <p>Following S,O from SOLID</p>
+ * <p>Note: uncople Rond and RoundResult classes</p>
+ * @author Angel
+ */
+public class ScissorsStrategy implements IWinnerStrategy {
+
+    /**
+     * <strong>isWinner</strong> Determinates winner in a Scissors way
+     * @param second the second player move
+     * @return the winner
+     */
+    @Override
+    public String isWinner(String second) {
+        String sRes = "DRAW";
+        
+        if (second.toUpperCase().equals("ROCK")) {
+            sRes = "SECOND";
+        } else {
+            if (second.toUpperCase().equals("PAPER")) {
+                sRes = "FIRST";
+            } else {
+                if (!second.toUpperCase().equals("SCISSORS")) {
+                    sRes = "UNKNOWED";
+                }
+            }
+        }
+        
+        return sRes;
+    }
+    
+}
